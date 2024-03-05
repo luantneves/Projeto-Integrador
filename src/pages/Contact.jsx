75f7208a6a154fb4fd253dd04a725738ebada5bf
luantneves <luan.tneves@hotmail.com>
2024-03-04 22:19:27 -0300

    add useState in contact and  change picture home

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,21 @@
+import { useState } from 'react';
 import './Contact.css';
 
 export default function Contact(){
+
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [fone, setFone] = useState("");
+    const [cep, setCep] = useState("");
+    const [descricao, setDescricao] = useState("");
+    const [material, setMaterial] = useState("");
+    const [image, setImage] = useState("");  
+    
+
     function sendform(e){
         e.preventDefault();
-        alert("Solicitação enviada com sucesso!")
+        alert("Solicitação enviada com sucesso!");
+        console.log({name, email, fone, cep, descricao, material, image})
     }
 
     return (
@@ -13,24 +25,63 @@ export default function Contact(){
                 <form onSubmit={sendform}>
                    <fieldset>
                         <legend>Contato</legend>
-                        <input type="text" placeholder='Nome' />
-                        <input type="text" placeholder='E-mail'/>
-                        <input type="text" placeholder='Telefone'/>
-                        <input type="text" placeholder='CEP'/>
-                        <textarea name="" id="" cols="30" rows="10" placeholder='Descreva os materiais que deseja doar e anexe imagens. Podemos entrar em contato para entender melhor e agendar o melhor horário e local para retirada.'></textarea>
-                        <select name="materiais" id="">
-                            <option value="">Eletrônicos</option>
-                            <option value="">Móveis</option>
-                            <option value="">Papel / Papelão</option>
-                            <option value="">Plástico</option>
-                            <option value="">Vidro</option>
-                            <option value="">Óleo de cozinha</option>
-                            <option value="">Pneus</option>                            
+                        <input type="text"
+                        placeholder='Nome'
+                        required
+                        value={name}
+                        onChange={(event) => setName(event.target.value)}
+                        />
+
+                        <input type="text"
+                        placeholder='E-mail'
+                        required
+                        value={email}
+                        onChange={(event) => setEmail(event.target.value)}
+                        />
+
+                        <input type="text"
+                        placeholder='Telefone'
+                        required
+                        value={fone}
+                        onChange={(event) => setFone(event.target.value)}
+                        />
+
+                        <input type="text"
+                        placeholder='CEP'
+                        required
+                        value={cep}
+                        onChange={(event) => setCep(event.target.value)}
+                        />
+                        
+                        <textarea name="" id="" cols="30" rows="10"
+                        placeholder='Descreva os materiais que deseja doar e anexe imagens.
+                        Podemos entrar em contato para entender melhor e agendar o melhor horário e local para retirada.'
+                        required
+                        value={descricao}
+                        onChange={(event) => setDescricao(event.target.value)}
+                        />
+
+                       
+
+                        <div>Selecione o tipo do material:</div>
+                        <select name="materiais" id="" onChange={(e)=> {
+                            const selectMaterial = e.target.value;
+                            setMaterial(selectMaterial);
+                            
+                        }}>
+                            <option value="Eletrônicos">Eletrônicos</option>
+                            <option value="Móveis">Móveis</option>
+                            <option value="Papel / Papelão">Papel / Papelão</option>
+                            <option value="Plástico">Plástico</option>
+                            <option value="Vidro">Vidro</option>
+                            <option value="Óleo de cozinha">Óleo de cozinha</option>
+                            <option value="Pneus">Pneus</option>                            
                         </select>
                         <div>
-                            <label>Anexos:</label>
-                            <input type="file"/>
-                        </div>                        
+                            <label>Fotos: (Opcional)</label>
+                            <input type="file" name="image" onChange={e => setImage(e.target.files[0])}/>
+                        </div>                 
+                            
                         <button>Enviar</button>
                     
                     </fieldset>          
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import Perfil from '../components/Perfil/Perfil.jsx';
 import Img01 from '../imagens/img-01.png';
 import Img02 from '../imagens/img-02.png';
-import Img03 from '../imagens/img-03.png';
+import Img03 from '../imagens/entregador.jpg';
 import Titulo from '../imagens/titulo.png';
 import Materiais from '../imagens/materiais-aceitos.png';
 
@@ -16,8 +16,8 @@ export default function Home(props){
   
         <div className='Perfis'>
               <Perfil imagem={Img01} titulo="Quero doar materiais" texto="Separe os materiais por tipo de reciclagem. Não se preocupe que temos um guia para isso!"></Perfil>       
-              <Perfil imagem={Img02} titulo="Quero receber materiais" texto="Cadastre sua empresa ou cooperativa e receba os materiais coletados próximos!"></Perfil>       
-              <Perfil imagem={Img03} titulo="Quero fazer a entrega" texto=" Colete e entregue os materiais e contribua com a sustentabilidade!"></Perfil>     
+              <Perfil imagem={Img02} titulo="Quero receber materiais" texto="Cadastre sua empresa ou cooperativa e receba os materiais coletados próximos!" picture={false}></Perfil>       
+              <Perfil imagem={Img03} titulo="Quero fazer uma entrega" texto=" Colete e entregue os materiais e contribua com a sustentabilidade!"></Perfil>     
         </div>
   
         <a href="/about"><img src={Materiais} alt="" /></a>
